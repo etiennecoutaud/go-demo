b3c51af937af941c303ffe4930bd241bc13de33f
Etienne Coutaud <e.coutaud@gmail.com>
2018-07-26 11:32:38 +0200

    legacy branch

--- main.go
+++ main.go
@@ -7,14 +7,7 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
-
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	io.WriteString(w, `{"alive": true}`)
-}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -24,8 +17,6 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/health", healthCheckHandler)
-	r.Handle("/metrics", promhttp.Handler())
 	r.HandleFunc("/", homeHandler)
 
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
